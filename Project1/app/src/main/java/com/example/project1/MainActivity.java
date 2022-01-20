569a0a4bed4a82a0ebfc9de79aa99595422dc4e9
kkldream <kk69347321@gmail.com>
2022-01-20 13:24:38 +0800

    demo ok

--- Project1/app/src/main/java/com/example/project1/MainActivity.java
+++ Project1/app/src/main/java/com/example/project1/MainActivity.java
@@ -1,14 +1,107 @@
 package com.example.project1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.hardware.usb.UsbDevice;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.TextureView;
+import android.widget.Toast;
+
+import com.lgh.uvccamera.UVCCameraProxy;
+import com.lgh.uvccamera.callback.ConnectCallback;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    // Permissions
+    private static final String[] REQUIRED_PERMISSION_LIST = new String[]{
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.RECORD_AUDIO,
+    };
+    private static final int REQUEST_CODE = 1;
+    private List<String> mMissPermissions = new ArrayList<>();
+    // UVC
+    UVCCameraProxy mUVCCamera;
+    // View
+    TextureView mTextureView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        checkAndRequestPermissions();
+        mTextureView = findViewById(R.id.textureView);
+        mUVCCamera = new UVCCameraProxy(this);
+        mUVCCamera.setPreviewTexture(mTextureView);
+        mUVCCamera.setConnectCallback(new ConnectCallback() {
+            @Override
+            public void onAttached(UsbDevice usbDevice) {
+                mUVCCamera.requestPermission(usbDevice); // USB设备授权
+            }
+
+            @Override
+            public void onGranted(UsbDevice usbDevice, boolean granted) {
+                if (granted) {
+                    mUVCCamera.connectDevice(usbDevice); // 连接USB设备
+                }
+            }
+
+            @Override
+            public void onConnected(UsbDevice usbDevice) {
+                mUVCCamera.openCamera(); // 打开相机
+            }
+
+            @Override
+            public void onCameraOpened() {
+                mUVCCamera.setPreviewSize(640, 480); // 设置预览尺寸
+                mUVCCamera.startPreview(); // 开始预览
+            }
+
+            @Override
+            public void onDetached(UsbDevice usbDevice) {
+                mUVCCamera.closeCamera(); // 关闭相机
+            }
+        });
+
+    }
+
+    private void checkAndRequestPermissions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            mMissPermissions.clear();
+            for (String permission : REQUIRED_PERMISSION_LIST) {
+                int result = ContextCompat.checkSelfPermission(this, permission);
+                if (result != PackageManager.PERMISSION_GRANTED) {
+                    mMissPermissions.add(permission);
+                }
+            }
+            if (!mMissPermissions.isEmpty()) {
+                ActivityCompat.requestPermissions(this,
+                        mMissPermissions.toArray(new String[mMissPermissions.size()]),
+                        REQUEST_CODE);
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(final int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == REQUEST_CODE) {
+            for (int i = grantResults.length - 1; i >= 0; i--) {
+                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
+                    mMissPermissions.remove(permissions[i]);
+                }
+            }
+        }
+        if (!mMissPermissions.isEmpty()) {
+            Toast.makeText(this, "get permissions failed,exiting...",Toast.LENGTH_SHORT).show();
+            this.finish();
+        }
     }
 }
